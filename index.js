032cc5d77d5cf7f3797b5dc308136b778b15c4d2
Darian Fultz <dfultz826@yahoo.com>
2022-11-21 16:11:43 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(driverArr, driverNam){
+    return driverArr.filter(
+        listNames => listNames.toLowerCase() === driverNam.toLowerCase()
+    );
+}
+
+function fuzzyMatch(driverArr, driverLetters){
+    return driverArr.filter(
+        maybeNames => maybeNames.toLowerCase().indexOf(driverLetters.toLowerCase()) === 0
+    );
+}
+function matchName (driverArr, nameSeek){
+    return driverArr.filter(drvElement => drvElement.name === nameSeek
+    );
+}
